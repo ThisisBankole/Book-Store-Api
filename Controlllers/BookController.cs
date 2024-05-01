1b886cd5ca55077113f811dbf36be62bcad65a44
thisisbankole <bjamgbadi@gmail.com>
2024-05-02 00:11:32 +0100

    initial push for bookstore api

---
+++ Controlllers/BookController.cs
@@ -0,0 +1,75 @@
+﻿using BookStoreApi.Models;
+using BookStoreApi.Services;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace BookStoreApi.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class BookController : ControllerBase
+{
+    private readonly BooksService _booksService;
+
+    public BookController(BooksService booksService)
+    {
+        _booksService = booksService;
+    }
+
+    [HttpGet]
+    public async Task<List<Book>> Get() =>
+        await _booksService.GetBooksAsync();
+
+    [HttpGet("{id:length(24)}")]
+    public async Task<ActionResult<Book>> Get(string id)
+    {
+        var book = await _booksService.GetBookAsync(id);
+
+        if (book == null)
+        {
+            return NotFound();
+        }
+        return book;
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Create(Book book)
+    {
+        await _booksService.CreateBookAsync(book);
+
+        return CreatedAtAction(nameof(Get), new { id = book.Id }, book);
+    }
+
+    [HttpPut("{id:length(24)}")]
+    public async Task<IActionResult> Update(string id, Book bookIn)
+    {
+        var book = await _booksService.GetBookAsync(id);
+
+        if (book == null)
+        {
+            return NotFound();
+        }
+
+        bookIn.Id = book.Id;
+
+        await _booksService.UpdateBookAsync(id, bookIn);
+
+        return NoContent();
+    }
+
+    [HttpDelete("{id:length(24)}")]
+    public async Task<IActionResult> Delete(string id)
+    {
+        var book = await _booksService.GetBookAsync(id);
+
+        if (book == null)
+        {
+            return NotFound();
+        }
+        await _booksService.DeleteBookAsync(id);
+
+        return NoContent();
+    }
+
+
+}
---
+++ Services/BooksService.cs
@@ -0,0 +1,37 @@
+﻿using BookStoreApi.Models;
+using Microsoft.Extensions.Options;
+using MongoDB.Driver;
+
+namespace BookStoreApi.Services;
+
+public class BooksService
+{
+    private readonly IMongoCollection<Book> _booksCollection;
+
+    public BooksService(IOptions<BookstoreDatabaseSettings> bookStoreDatabaseSettings)
+    {
+        var mongoClient = new MongoClient(bookStoreDatabaseSettings.Value.ConnectionString);
+
+        var mongoDatabase = mongoClient.GetDatabase(bookStoreDatabaseSettings.Value.DatabaseName);
+
+        _booksCollection = mongoDatabase.GetCollection<Book>(bookStoreDatabaseSettings.Value.BooksCollectionName);
+    }
+    public async Task<List<Book>> GetBooksAsync() =>
+        await _booksCollection.Find(_ => true).ToListAsync();
+    
+
+    public async Task<Book?> GetBookAsync(string id)=>
+        await _booksCollection.Find(book => book.Id == id).FirstOrDefaultAsync();
+
+    
+    public async Task CreateBookAsync(Book newBook) =>
+        await _booksCollection.InsertOneAsync(newBook);
+
+    public async Task UpdateBookAsync(string id, Book updatedBook) =>
+        await _booksCollection.ReplaceOneAsync(book => book.Id == id, updatedBook);
+
+    public async Task DeleteBookAsync(string id) =>
+        await _booksCollection.DeleteOneAsync(book => book.Id == id);
+
+
+}
